145bfec091a9a3b56adf209a2a36a65b598ac320
sohayla <najibsoula5@gmail.com>
2025-01-08 20:58:58 +0200

    step 4

--- movie-database/index.js
+++ movie-database/index.js
@@ -17,3 +17,22 @@ app.get('/time', (req, res) => {
     const currentTime = new Date().toLocaleTimeString();
     res.json({ status: 200, message: currentTime });
 });
+app.get('/test', (req, res) => {
+    res.json({ status: 200, message: "ok" });
+});
+app.get('/time', (req, res) => {
+    const time = new Date().toLocaleTimeString();
+    res.json({ status: 200, message: time });
+});
+app.get('/hello/:id?', (req, res) => {
+    const id = req.params.id || "Guest";
+    res.json({ status: 200, message: `Hello, ${id}` });
+});
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+    if (search) {
+        res.json({ status: 200, message: "ok", data: search });
+    } else {
+        res.status(500).json({ status: 500, error: true, message: "you have to provide a search" });
+    }
+});
