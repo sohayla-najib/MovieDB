fbc0a0a8e9fabb31cea86d342667c7b5ba84be3e
sohayla <najibsoula5@gmail.com>
2025-01-08 21:14:13 +0200

    step 7

--- movie-database/index.js
+++ movie-database/index.js
@@ -40,7 +40,7 @@ const movies = [
     { title: 'Jaws', year: 1975, rating: 8 },
     { title: 'Avatar', year: 2009, rating: 7.8 },
     { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+    { title:'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ];
 app.get('/movies/read', (req, res) => {
     res.json({ status: 200, data: movies });
